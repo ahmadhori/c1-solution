5e277c8b572fa943a5eff4f96413b381640d7fcf
Ahmad Houri <houri@future-demand.com>
2021-08-24 03:05:43 +0200

    using Jinja2Templates in an endpoint

--- application.py
+++ application.py
@@ -1,13 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
 import uvicorn
-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/navigator")
+def read_item(search_term: Optional[str] = None):
+    return {"search_term": search_term}
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
 
 
 if __name__ == "__main__":
