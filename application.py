ab2a5197c51217ef83671e0d7233d285e529a794
Ahmad Houri <houri@future-demand.com>
2021-08-25 12:55:48 +0200

    Added documentation

--- application.py
+++ application.py
@@ -3,9 +3,10 @@ from typing import Optional
 
 import requests
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi import FastAPI, Request, Query
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+
 
 from github import Github
 
@@ -17,7 +18,19 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/navigator", response_class=HTMLResponse)
-def search_github(request: Request, search_term: str):
+def search_github(request: Request, search_term: str = Query(..., min_length=1)):
+    """[summary]
+    This endpoint will search for Github repositories using search term
+    Parameters
+    ----------
+    search_term : str
+        string that you want to search for (must be a non empty string)
+
+    Returns
+    -------
+    [type]
+        html page which shows to top results according to the task criteria
+    """
     github = Github()
     try:
         repos = github.find_repo(query=search_term, sort_key="created_at", reverse=True)[:5]
@@ -25,15 +38,19 @@ def search_github(request: Request, search_term: str):
             repo.last_commit = github.get_last_commit(repo)
     except requests.exceptions.HTTPError as err:
         return templates.TemplateResponse("error.html", {"request": request,
-                                                            "message":"Failed to fetch the data from github, please try again later..."})
+                                                         "message": "Failed to fetch the data from github, please try again later..."})
 
     return templates.TemplateResponse("template.html",
                                       {"request": request,
                                        "search_term": search_term,
                                        "repository_list": repos})
 
+
 @app.get("/")
 async def redirect_to_docs():
+    """[summary]
+    Redirect to the documentation page when accessing root
+    """
     response = RedirectResponse(url='/docs')
     return response
 
--- github.py
+++ github.py
@@ -7,11 +7,30 @@ from schema import Commit, Repository
 
 
 class Github:
+    """[summary]
+    This class is reposnsible to communicate with Github API and to return the results
+    """
 
     search_url = 'https://api.github.com/search/repositories'
     commits_url = 'https://api.github.com/repos/{owner}/{repo}/commits'
 
     def find_repo(self, query: str, sort_key=None, reverse=True) -> List[Repository]:
+        """[summary]
+
+        Parameters
+        ----------
+        query : str
+            Search query
+        sort_key : [type], optional
+            you can provide attribute name to sort by it, by default None
+        reverse : bool, optional
+            order of sorting, by default True which means desc order
+
+        Returns
+        -------
+        List[Repository]
+            list of repositories based on search term
+        """
         response = requests.get(Github.search_url, params={'q': query})
         response.raise_for_status()
         repos = parse_obj_as(List[Repository], response.json()['items'])
@@ -20,6 +39,18 @@ class Github:
         return repos
 
     def get_last_commit(self, repo: Repository) -> Commit:
+        """[summary]
+
+        Parameters
+        ----------
+        repo : Repository
+            repository which you want to fetch the commits for it
+
+        Returns
+        -------
+        Commit
+            returns a commit object according to the schema
+        """
         response = requests.get(Github.commits_url.format(owner=repo.owner.login, repo=repo.repository_name), params={"per_page": 1})
         response.raise_for_status()
         if len(response.json()) > 0:
