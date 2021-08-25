e20bee59d6b07101f2d15b469fa61b85ebc90f61
Ahmad Houri <houri@future-demand.com>
2021-08-25 02:58:14 +0200

    Added pydentic schemas

---
+++ schema.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+
+
+
+class Commit(BaseModel):
+    sha: str
+    author_name: str
+    message: str
+
+    def __init__(self, **kwargs):
+        kwargs["author_name"] = kwargs["commit"]["author"]["name"]
+        kwargs["message"] = kwargs["commit"]["message"]
+        super().__init__(**kwargs)
+
+
+
+class Owner(BaseModel):
+    html_url: str
+    avatar_url: str
+    login: str
+
+
+
+class Repository(BaseModel):
+    repo_id: int = Field(alias='id')
+    repository_name: str = Field(alias='name')
+    created_at: datetime
+    owner: Owner
+    last_commit: Optional[Commit] = None
